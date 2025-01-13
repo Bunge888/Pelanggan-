95349ee2b22722afc137a5d8c8f7a33cc274fc43
Bunge888 <burhandoang59@gmail.com>
2025-01-13 10:07:11 +0700

    update main.js

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log('Hello World!');
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDocs,
+  getDoc,
+  addDoc,
+  deleteDoc,
+  updateDoc,
+  query,
+  orderBy
+} from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCbqYHEkbK8rlmTEvT7IRxamrueBVOvf2o",
+  authDomain: "insan-cemerlang-25a53.firebaseapp.com",
+  projectId: "insan-cemerlang-25a53",
+  storageBucket: "insan-cemerlang-25a53.appspot.com",
+  messagingSenderId: "383282706725",
+  appId: "1:383282706725:web:bf9fd56cea37cb3a0fc2cc",
+  measurementId: "G-31EQF6PDVN"
+};
+
